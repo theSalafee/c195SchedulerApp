ade403937a7cf34c2e1aae261820d8e5e3d63144
Naasir Salafee <naasirbush@icloud.com>
2020-03-08 14:40:08 -0700

    Initial commit

---
+++ src/sample/MainMenu.java
@@ -0,0 +1,33 @@
+package sample;
+
+import javafx.event.ActionEvent;
+import javafx.scene.control.Button;
+import javafx.scene.control.ToolBar;
+
+/**
+ * FXML Controller class
+ *
+ * @author Naasir Bush
+ */
+public class MainMenu {
+
+    public Button customersBtn;
+    public Button appointmentsBtn;
+    public Button reportsBtn;
+
+    public void customerBtnHandler(ActionEvent actionEvent) {
+        // TODO: 3/8/20 Hook-up customer btn
+        // TODO: 3/8/20 create customerView.fxml
+
+    }
+
+    public void appointmentBtnHandler(ActionEvent actionEvent) {
+        // TODO: 3/8/20 Hook-up appointment btn 
+        // TODO: 3/8/20 create appointments view 
+    }
+
+    public void reportsBtnHandler(ActionEvent actionEvent) {
+        // TODO: 3/8/20 Hook-up reports btn 
+        // TODO: 3/8/20 create reports view
+    }
+}
