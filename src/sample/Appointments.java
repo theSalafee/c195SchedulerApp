415661fc9d9e6eaa0635b495e07bf7ef664fddbd
Naasir Salafee <naasirbush@icloud.com>
2020-03-11 19:38:53 -0700

    Added appointments.fxml, Appointments.java, and models folder

---
+++ src/sample/Appointments.java
@@ -0,0 +1,26 @@
+package sample;
+
+import javafx.event.ActionEvent;
+import javafx.scene.control.ComboBox;
+import javafx.scene.control.DatePicker;
+import javafx.scene.control.TextField;
+
+public class Appointments {
+
+    public ComboBox AppointmentCustomerField;
+    public ComboBox AppointmentDescField;
+    public TextField AppointmentTitleField;
+    public TextField AppointmentContactField;
+    public TextField AppointmentURLField;
+    public TextField AppointmentLocationField;
+    public DatePicker AppointmentDateField;
+    public ComboBox AppointmentStartField;
+    public ComboBox AppointmentEndField;
+
+
+    public void saveHandler(ActionEvent actionEvent) {
+    }
+
+    public void cancelHandler(ActionEvent actionEvent) {
+    }
+}
