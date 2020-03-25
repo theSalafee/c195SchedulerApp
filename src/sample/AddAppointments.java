51007b12ea8b6867f046120e78df34724c3cd6e9
Naasir Salafee <naasirbush@icloud.com>
2020-03-24 20:32:19 -0700

    added AddAppointments fxml/java and adapted Appointments fxml/java

---
+++ src/sample/AddAppointments.java
@@ -0,0 +1,27 @@
+package sample;
+
+import javafx.event.ActionEvent;
+import javafx.scene.control.Button;
+import javafx.scene.control.ComboBox;
+import javafx.scene.control.DatePicker;
+import javafx.scene.control.TextField;
+
+public class AddAppointments {
+
+    public ComboBox AppointmentDescField;
+    public DatePicker AppointmentDateField;
+    public ComboBox AppointmentStartField;
+    public ComboBox AppointmentEndField;
+    public Button saveBtn;
+    public Button cancelBtn;
+    public ComboBox contactField;
+    public TextField locationField;
+    public TextField customerName;
+
+
+    public void saveHandler(ActionEvent actionEvent) {
+    }
+
+    public void cancelHandler(ActionEvent actionEvent) {
+    }
+}
--- src/sample/Appointments.java
+++ src/sample/Appointments.java
@@ -1,26 +1,36 @@
 package sample;
 
 import javafx.event.ActionEvent;
-import javafx.scene.control.ComboBox;
-import javafx.scene.control.DatePicker;
-import javafx.scene.control.TextField;
+import javafx.scene.control.Button;
+import javafx.scene.control.TableColumn;
+import javafx.scene.control.TableView;
+import javafx.scene.layout.AnchorPane;
 
 public class Appointments {
+    public Button backBtn;
+    public Button addAppointmentBtn;
+    public Button modifyCustomerBtn;
+    public Button deleteAppointmentBtn;
+    public TableView customerTable;
+    public TableColumn custName;
+    public TableColumn custAddress1;
+    public TableColumn custAddress2;
+    public TableColumn custCity;
+    public TableColumn custCounty;
+    public TableColumn custPhone;
+    public TableColumn custPostalCode;
+    public AnchorPane appointmentsView;
+    public TableView appointmentsTable;
 
-    public ComboBox AppointmentCustomerField;
-    public ComboBox AppointmentDescField;
-    public TextField AppointmentTitleField;
-    public TextField AppointmentContactField;
-    public TextField AppointmentURLField;
-    public TextField AppointmentLocationField;
-    public DatePicker AppointmentDateField;
-    public ComboBox AppointmentStartField;
-    public ComboBox AppointmentEndField;
-
-
-    public void saveHandler(ActionEvent actionEvent) {
+    public void backBtnHandler(ActionEvent actionEvent) {
     }
 
-    public void cancelHandler(ActionEvent actionEvent) {
+    public void addHandler(ActionEvent actionEvent) {
+    }
+
+    public void modifyHandler(ActionEvent actionEvent) {
+    }
+
+    public void deleteHandler(ActionEvent actionEvent) {
     }
 }
