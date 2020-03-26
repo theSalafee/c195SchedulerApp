f1922f279b8fb6c7f8e3670c121b12c996dc86d9
Naasir Salafee <naasirbush@icloud.com>
2020-03-25 19:33:05 -0700

    login.fxml and Login.java

---
+++ src/sample/Login.java
@@ -0,0 +1,12 @@
+package sample;
+
+import javafx.event.ActionEvent;
+import javafx.scene.control.TextField;
+
+public class Login {
+    public TextField userName;
+    public TextField password;
+
+    public void loginHandler(ActionEvent actionEvent) {
+    }
+}
