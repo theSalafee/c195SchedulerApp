27f510300ab8075233e5eba99a889e3bae3c5ad7
Naasir Salafee <naasirbush@icloud.com>
2020-03-25 21:20:20 -0700

    AddCustomers java and fxml

--- src/sample/AddCustomers.java
+++ src/sample/AddCustomers.java
@@ -1,4 +1,23 @@
 package sample;
 
+import javafx.event.ActionEvent;
+import javafx.scene.control.Button;
+import javafx.scene.control.TextField;
+
 public class AddCustomers {
+    public TextField addressTwo;
+    public Button saveBtn;
+    public Button cancelBtn;
+    public TextField customerName;
+    public TextField addressOne;
+    public TextField city;
+    public TextField country;
+    public TextField phone;
+    public TextField postalCode;
+
+    public void saveHandler(ActionEvent actionEvent) {
+    }
+
+    public void cancelHandler(ActionEvent actionEvent) {
+    }
 }
