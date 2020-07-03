a9f4a75fa22f016ac1109412c5ecec31037a91b1
Naasir Salafee <naasirbush@icloud.com>
2020-07-03 11:33:29 -0700

    changed start/end time combo box length. Fixed bug for picking 9:00 and 9:15

--- src/viewAndController/AddAppointments.java
+++ src/viewAndController/AddAppointments.java
@@ -63,7 +63,7 @@ public class AddAppointments implements Initializable {
     public void initialize(URL url, ResourceBundle rb) {
         isNewAppointment = AppointmentController.isNewAppointment;
         AppointmentStartField.getItems().addAll("08:00", "08:15", "08:30", "08:45", "09:00", "09:15",
-                "9:30", "9:45", "10:00", "10:15", "10:30", "10:45", "11:00", "11:15", "11:30", "11:45",
+                "09:30", "09:45", "10:00", "10:15", "10:30", "10:45", "11:00", "11:15", "11:30", "11:45",
                 "12:00", "12:15", "12:30", "12:45", "13:00", "13:15", "13:30", "13:45", "14:00", "14:15",
                 "14:30", "14:45", "15:00", "15:15", "15:30", "15:45", "16:00", "16:15", "16:30", "16:45");
         AppointmentEndField.getItems().addAll("08:15", "08:30", "08:45", "09:00", "09:15",
